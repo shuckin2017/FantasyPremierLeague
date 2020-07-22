cb61aa928e669eeedb79a0fd011bd1fdc913466d
shuckin2017 <shuckin@mail.ru>
2020-07-23 01:37:45 +0300

    Создали таблицу

---
+++ src/js/main.js
@@ -0,0 +1,1071 @@
+var data = [
+  {
+    "name": "Albrighton",
+    "team": "Leicestercester",
+    "cost": "5.1",
+    "overall": "36"
+},
+{
+    "name": "Alli",
+    "team": "TOT",
+    "cost": "8.3",
+    "overall": "116"
+},
+{
+    "name": "Almirón",
+    "team": "NEW",
+    "cost": "5.8",
+    "overall": "102"
+},
+{
+    "name": "Bernard",
+    "team": "EVE",
+    "cost": "6.4",
+    "overall": "68"
+},
+{
+    "name": "Antonio",
+    "team": "WHU",
+    "cost": "7.0",
+    "overall": "95"
+},
+{
+    "name": "Armstrong",
+    "team": "SOU",
+    "cost": "5.2",
+    "overall": "93"
+},
+{
+    "name": "Atsu",
+    "team": "NEW",
+    "cost": "5.3",
+    "overall": "37"
+},
+{
+    "name": "Barkley",
+    "team": "CHE",
+    "cost": "5.6",
+    "overall": "59"
+},
+{
+    "name": "Barnes (Harvey)",
+    "team": "Leicestercester",
+    "cost": "5.9",
+    "overall": "127"
+},
+{
+    "name": "Bentaleb",
+    "team": "NEW",
+    "cost": "4.7",
+    "overall": "13"
+},
+{
+    "name": "Gudmundsson",
+    "team": "BUR",
+    "cost": "5.9",
+    "overall": "22"
+},
+{
+    "name": "Berge",
+    "team": "SHU",
+    "cost": "4.7",
+    "overall": "35"
+},
+{
+    "name": "Bergwijn",
+    "team": "TOT",
+    "cost": "7.0",
+    "overall": "37"
+},
+{
+    "name": "Billing",
+    "team": "BOU",
+    "cost": "4.9",
+    "overall": "64"
+},
+{
+    "name": "Bissouma",
+    "team": "BHA",
+    "cost": "5.0",
+    "overall": "29"
+},
+{
+    "name": "Willian",
+    "team": "CHE",
+    "cost": "7.4",
+    "overall": "161"
+},
+{
+    "name": "Bruno Fernandes",
+    "team": "MUN",
+    "cost": "9.0",
+    "overall": "99"
+},
+{
+    "name": "Boufal",
+    "team": "SOU",
+    "cost": "5.5",
+    "overall": "34"
+},
+{
+    "name": "Bowen",
+    "team": "WHU",
+    "cost": "6.4",
+    "overall": "36"
+},
+{
+    "name": "Brooks",
+    "team": "BOU",
+    "cost": "6.1",
+    "overall": "11"
+},
+{
+    "name": "Brownhill",
+    "team": "BUR",
+    "cost": "4.8",
+    "overall": "15"
+},
+{
+    "name": "Buendía",
+    "team": "NOR",
+    "cost": "6.0",
+    "overall": "96"
+},
+{
+    "name": "Cantwell",
+    "team": "NOR",
+    "cost": "4.5",
+    "overall": "106"
+},
+{
+    "name": "Capoue",
+    "team": "WAT",
+    "cost": "4.8",
+    "overall": "78"
+},
+{
+    "name": "Gedson Fernandes",
+    "team": "TOT",
+    "cost": "4.6",
+    "overall": "6"
+},
+{
+    "name": "Daniel Podence",
+    "team": "WOL",
+    "cost": "4.9",
+    "overall": "11"
+},
+{
+    "name": "Dani Ceballos",
+    "team": "ARS",
+    "cost": "5.2",
+    "overall": "53"
+},
+{
+    "name": "Chalobah (Nathaniel)",
+    "team": "WAT",
+    "cost": "4.4",
+    "overall": "33"
+},
+{
+    "name": "Choudhury",
+    "team": "Leicestercester",
+    "cost": "4.3",
+    "overall": "32"
+},
+{
+    "name": "Cleverley",
+    "team": "WAT",
+    "cost": "4.8",
+    "overall": "29"
+},
+{
+    "name": "L Cook",
+    "team": "BOU",
+    "cost": "5.0",
+    "overall": "39"
+},
+{
+    "name": "Cork",
+    "team": "BUR",
+    "cost": "4.9",
+    "overall": "70"
+},
+{
+    "name": "Ruben Neves",
+    "team": "WOL",
+    "cost": "5.2",
+    "overall": "91"
+},
+{
+    "name": "Danjuma Groeneveld",
+    "team": "BOU",
+    "cost": "5.8",
+    "overall": "24"
+},
+{
+    "name": "Davies (Tom)",
+    "team": "EVE",
+    "cost": "5.3",
+    "overall": "53"
+},
+{
+    "name": "Davis",
+    "team": "AVL",
+    "cost": "4.3",
+    "overall": "22"
+},
+{
+    "name": "Richarlison",
+    "team": "EVE",
+    "cost": "8.2",
+    "overall": "155"
+},
+{
+    "name": "De Bruyne",
+    "team": "MCI",
+    "cost": "10.8",
+    "overall": "226"
+},
+{
+    "name": "Dendoncker",
+    "team": "WOL",
+    "cost": "4.4",
+    "overall": "95"
+},
+{
+    "name": "Dier",
+    "team": "TOT",
+    "cost": "4.8",
+    "overall": "35"
+},
+{
+    "name": "Doucouré",
+    "team": "WAT",
+    "cost": "5.5",
+    "overall": "104"
+},
+{
+    "name": "Doyle",
+    "team": "MCI",
+    "cost": "4.5",
+    "overall": "1"
+},
+{
+    "name": "Duda",
+    "team": "NOR",
+    "cost": "4.4",
+    "overall": "24"
+},
+{
+    "name": "El Ghazi",
+    "team": "AVL",
+    "cost": "5.3",
+    "overall": "91"
+},
+{
+    "name": "Elliott",
+    "team": "LIV",
+    "cost": "4.5",
+    "overall": "2"
+},
+{
+    "name": "Fleck",
+    "team": "SHU",
+    "cost": "4.7",
+    "overall": "98"
+},
+{
+    "name": "Foden",
+    "team": "MCI",
+    "cost": "5.5",
+    "overall": "58"
+},
+{
+    "name": "Fornals",
+    "team": "WHU",
+    "cost": "6.0",
+    "overall": "83"
+},
+{
+    "name": "Freeman (Luke)",
+    "team": "SHU",
+    "cost": "5.0",
+    "overall": "13"
+},
+{
+    "name": "Jorginho",
+    "team": "CHE",
+    "cost": "4.9",
+    "overall": "84"
+},
+{
+    "name": "Gibbs-White",
+    "team": "WOL",
+    "cost": "4.7",
+    "overall": "11"
+},
+{
+    "name": "Gilmour",
+    "team": "CHE",
+    "cost": "4.2",
+    "overall": "9"
+},
+{
+    "name": "Gosling",
+    "team": "BOU",
+    "cost": "4.8",
+    "overall": "49"
+},
+{
+    "name": "Gray (Demarai)",
+    "team": "Leicestercester",
+    "cost": "5.0",
+    "overall": "36"
+},
+{
+    "name": "Grealish",
+    "team": "AVL",
+    "cost": "5.9",
+    "overall": "133"
+},
+{
+    "name": "Groß",
+    "team": "BHA",
+    "cost": "6.2",
+    "overall": "75"
+},
+{
+    "name": "Guendouzi",
+    "team": "ARS",
+    "cost": "4.1",
+    "overall": "48"
+},
+{
+    "name": "Gündogan",
+    "team": "MCI",
+    "cost": "5.1",
+    "overall": "61"
+},
+{
+    "name": "Trezeguet",
+    "team": "AVL",
+    "cost": "5.2",
+    "overall": "83"
+},
+{
+    "name": "Hayden",
+    "team": "NEW",
+    "cost": "4.4",
+    "overall": "72"
+},
+{
+    "name": "Henderson (Jordan)",
+    "team": "LIV",
+    "cost": "5.2",
+    "overall": "116"
+},
+{
+    "name": "Hernández (Onel)",
+    "team": "NOR",
+    "cost": "5.3",
+    "overall": "46"
+},
+{
+    "name": "Rodri",
+    "team": "MCI",
+    "cost": "5.3",
+    "overall": "87"
+},
+{
+    "name": "Hourihane",
+    "team": "AVL",
+    "cost": "5.7",
+    "overall": "70"
+},
+{
+    "name": "Hudson-Odoi",
+    "team": "CHE",
+    "cost": "5.1",
+    "overall": "47"
+},
+{
+    "name": "Hughes",
+    "team": "WAT",
+    "cost": "5.3",
+    "overall": "59"
+},
+{
+    "name": "Højbjerg",
+    "team": "SOU",
+    "cost": "4.8",
+    "overall": "72"
+},
+{
+    "name": "Iwobi",
+    "team": "EVE",
+    "cost": "5.6",
+    "overall": "49"
+},
+{
+    "name": "Jahanbakhsh",
+    "team": "BHA",
+    "cost": "5.8",
+    "overall": "27"
+},
+{
+    "name": "James (Daniel)",
+    "team": "MUN",
+    "cost": "5.7",
+    "overall": "95"
+},
+{
+    "name": "Jones (Curtis)",
+    "team": "LIV",
+    "cost": "4.5",
+    "overall": "11"
+},
+{
+    "name": "Kanté",
+    "team": "CHE",
+    "cost": "5.0",
+    "overall": "57"
+},
+{
+    "name": "Keita",
+    "team": "LIV",
+    "cost": "5.8",
+    "overall": "42"
+},
+{
+    "name": "Kouyaté",
+    "team": "CRY",
+    "cost": "4.9",
+    "overall": "82"
+},
+{
+    "name": "Kovacic",
+    "team": "CHE",
+    "cost": "5.3",
+    "overall": "61"
+},
+{
+    "name": "Lamela",
+    "team": "TOT",
+    "cost": "5.7",
+    "overall": "60"
+},
+{
+    "name": "Lanzini",
+    "team": "WHU",
+    "cost": "6.1",
+    "overall": "49"
+},
+{
+    "name": "Lazaro",
+    "team": "NEW",
+    "cost": "5.3",
+    "overall": "15"
+},
+{
+    "name": "Lerma",
+    "team": "BOU",
+    "cost": "4.8",
+    "overall": "59"
+},
+{
+    "name": "Lo Celso",
+    "team": "TOT",
+    "cost": "7.3",
+    "overall": "39"
+},
+{
+    "name": "Loftus-Cheek",
+    "team": "CHE",
+    "cost": "5.9",
+    "overall": "8"
+},
+{
+    "name": "Pedro Neto",
+    "team": "WOL",
+    "cost": "5.0",
+    "overall": "55"
+},
+{
+    "name": "Longstaff (Matthew)",
+    "team": "NEW",
+    "cost": "4.5",
+    "overall": "26"
+},
+{
+    "name": "Longstaff (Sean)",
+    "team": "NEW",
+    "cost": "5.0",
+    "overall": "43"
+},
+{
+    "name": "Fernandinho",
+    "team": "MCI",
+    "cost": "5.1",
+    "overall": "52"
+},
+{
+    "name": "Alexis Mac Allister",
+    "team": "BHA",
+    "cost": "5.5",
+    "overall": "8"
+},
+{
+    "name": "Maddison",
+    "team": "Leicester",
+    "cost": "7.3",
+    "overall": "125"
+},
+{
+    "name": "Mahrez",
+    "team": "MCI",
+    "cost": "8.6",
+    "overall": "164"
+},
+{
+    "name": "Mane",
+    "team": "LIV",
+    "cost": "12.2",
+    "overall": "202"
+},
+{
+    "name": "Solly March",
+    "team": "BHA",
+    "cost": "4.6",
+    "overall": "33"
+},
+{
+    "name": "Martial",
+    "team": "MUN",
+    "cost": "8.4",
+    "overall": "181"
+},
+{
+    "name": "Martin (Josh)",
+    "team": "NOR",
+    "cost": "4.5",
+    "overall": "3"
+},
+{
+    "name": "Mata",
+    "team": "MUN",
+    "cost": "6.1",
+    "overall": "38"
+},
+{
+    "name": "Matic",
+    "team": "MUN",
+    "cost": "4.8",
+    "overall": "49"
+},
+{
+    "name": "McArthur",
+    "team": "CRY",
+    "cost": "5.2",
+    "overall": "86"
+},
+{
+    "name": "McCarthy (James)",
+    "team": "CRY",
+    "cost": "4.2",
+    "overall": "37"
+},
+{
+    "name": "McGinn",
+    "team": "AVL",
+    "cost": "5.3",
+    "overall": "74"
+},
+{
+    "name": "Kenny McLean",
+    "team": "NOR",
+    "cost": "4.8",
+    "overall": "71"
+},
+{
+    "name": "McNeil",
+    "team": "BUR",
+    "cost": "6.0",
+    "overall": "116"
+},
+{
+    "name": "McTominay",
+    "team": "MUN",
+    "cost": "4.8",
+    "overall": "70"
+},
+{
+    "name": "Mendy (Nampalys)",
+    "team": "Leicester",
+    "cost": "4.3",
+    "overall": "10"
+},
+{
+    "name": "Meyer",
+    "team": "CRY",
+    "cost": "5.2",
+    "overall": "23"
+},
+{
+    "name": "Milivojevic",
+    "team": "CRY",
+    "cost": "6.7",
+    "overall": "79"
+},
+{
+    "name": "Milner",
+    "team": "LIV",
+    "cost": "5.3",
+    "overall": "46"
+},
+{
+    "name": "Mooy",
+    "team": "BHA",
+    "cost": "4.7",
+    "overall": "77"
+},
+{
+    "name": "Mount",
+    "team": "CHE",
+    "cost": "6.1",
+    "overall": "122"
+},
+{
+    "name": "Moutinho",
+    "team": "WOL",
+    "cost": "5.4",
+    "overall": "106"
+},
+{
+    "name": "Nakamba",
+    "team": "AVL",
+    "cost": "4.4",
+    "overall": "41"
+},
+{
+    "name": "Ndidi",
+    "team": "Leicester",
+    "cost": "5.0",
+    "overall": "78"
+},
+{
+    "name": "NDombele",
+    "team": "TOT",
+    "cost": "5.8",
+    "overall": "45"
+},
+{
+    "name": "Nelson",
+    "team": "ARS",
+    "cost": "5.3",
+    "overall": "23"
+},
+{
+    "name": "Noble",
+    "team": "WHU",
+    "cost": "4.9",
+    "overall": "95"
+},
+{
+    "name": "Norwood",
+    "team": "SHU",
+    "cost": "4.7",
+    "overall": "94"
+},
+{
+    "name": "Osborn",
+    "team": "SHU",
+    "cost": "4.7",
+    "overall": "17"
+},
+{
+    "name": "Oxlade-Chamberlain",
+    "team": "LIV",
+    "cost": "6.1",
+    "overall": "63"
+},
+{
+    "name": "Jota",
+    "team": "AVL",
+    "cost": "5.7",
+    "overall": "17"
+},
+{
+    "name": "Pepe",
+    "team": "ARS",
+    "cost": "8.9",
+    "overall": "101"
+},
+{
+    "name": "Pereira",
+    "team": "MUN",
+    "cost": "4.8",
+    "overall": "60"
+},
+{
+    "name": "Felipe Anderson",
+    "team": "WHU",
+    "cost": "6.6",
+    "overall": "65"
+},
+{
+    "name": "Pereyra",
+    "team": "WAT",
+    "cost": "5.6",
+    "overall": "57"
+},
+{
+    "name": "Ayoze Pérez",
+    "team": "Leicester",
+    "cost": "6.0",
+    "overall": "119"
+},
+{
+    "name": "Pierrick",
+    "team": "CRY",
+    "cost": "4.5",
+    "overall": "2"
+},
+{
+    "name": "Pogba",
+    "team": "MUN",
+    "cost": "8.3",
+    "overall": "44"
+},
+{
+    "name": "Praet",
+    "team": "Leicester",
+    "cost": "5.2",
+    "overall": "52"
+},
+{
+    "name": "Pröpper",
+    "team": "BHA",
+    "cost": "4.8",
+    "overall": "74"
+},
+{
+    "name": "Pulisic",
+    "team": "CHE",
+    "cost": "7.3",
+    "overall": "109"
+},
+{
+    "name": "Pussetto",
+    "team": "WAT",
+    "cost": "4.6",
+    "overall": "6"
+},
+{
+    "name": "Redmond",
+    "team": "SOU",
+    "cost": "6.2",
+    "overall": "92"
+},
+{
+    "name": "Rice",
+    "team": "WHU",
+    "cost": "4.7",
+    "overall": "84"
+},
+{
+    "name": "Riedewald",
+    "team": "CRY",
+    "cost": "4.5",
+    "overall": "19"
+},
+{
+    "name": "Fred",
+    "team": "MUN",
+    "cost": "5.1",
+    "overall": "50"
+},
+{
+    "name": "Lucas Moura",
+    "team": "TOT",
+    "cost": "7.0",
+    "overall": "87"
+},
+{
+    "name": "Pedro",
+    "team": "CHE",
+    "cost": "6.8",
+    "overall": "31"
+},
+{
+    "name": "Rodwell",
+    "team": "SHU",
+    "cost": "4.5",
+    "overall": "1"
+},
+{
+    "name": "Romeu",
+    "team": "SOU",
+    "cost": "4.3",
+    "overall": "46"
+},
+{
+    "name": "Rupp",
+    "team": "NOR",
+    "cost": "4.4",
+    "overall": "14"
+},
+{
+    "name": "Saint-Maximin",
+    "team": "NEW",
+    "cost": "5.4",
+    "overall": "86"
+},
+{
+    "name": "Saka",
+    "team": "ARS",
+    "cost": "4.7",
+    "overall": "68"
+},
+{
+    "name": "Salah",
+    "team": "LIV",
+    "cost": "12.6",
+    "overall": "225"
+},
+{
+    "name": "Ismaila Sarr",
+    "team": "WAT",
+    "cost": "6.0",
+    "overall": "96"
+},
+{
+    "name": "Shelvey",
+    "team": "NEW",
+    "cost": "4.9",
+    "overall": "96"
+},
+{
+    "name": "Sigurdsson",
+    "team": "EVE",
+    "cost": "7.3",
+    "overall": "86"
+},
+{
+    "name": "Silva",
+    "team": "MCI",
+    "cost": "7.3",
+    "overall": "123"
+},
+{
+    "name": "Sissoko",
+    "team": "TOT",
+    "cost": "4.8",
+    "overall": "62"
+},
+{
+    "name": "Skipp",
+    "team": "TOT",
+    "cost": "4.3",
+    "overall": "5"
+},
+{
+    "name": "Douglas Luiz",
+    "team": "AVL",
+    "cost": "4.3",
+    "overall": "84"
+},
+{
+    "name": "Son",
+    "team": "TOT",
+    "cost": "9.6",
+    "overall": "153"
+},
+{
+    "name": "Soucek",
+    "team": "WHU",
+    "cost": "4.8",
+    "overall": "29"
+},
+{
+    "name": "Stanislas",
+    "team": "BOU",
+    "cost": "5.8",
+    "overall": "32"
+},
+{
+    "name": "Stephens (Dale)",
+    "team": "BHA",
+    "cost": "4.4",
+    "overall": "57"
+},
+{
+    "name": "Sterling (Raheem)",
+    "team": "MCI",
+    "cost": "11.8",
+    "overall": "178"
+},
+{
+    "name": "Stiepermann",
+    "team": "NOR",
+    "cost": "5.0",
+    "overall": "29"
+},
+{
+    "name": "Fabinho",
+    "team": "LIV",
+    "cost": "5.4",
+    "overall": "73"
+},
+{
+    "name": "André Gomes",
+    "team": "EVE",
+    "cost": "5.2",
+    "overall": "26"
+},
+{
+    "name": "Tella",
+    "team": "SOU",
+    "cost": "4.5",
+    "overall": "1"
+},
+{
+    "name": "Tettey",
+    "team": "NOR",
+    "cost": "4.4",
+    "overall": "63"
+},
+{
+    "name": "Tielemans",
+    "team": "Leicester",
+    "cost": "6.1",
+    "overall": "110"
+},
+{
+    "name": "Torreira",
+    "team": "ARS",
+    "cost": "4.7",
+    "overall": "45"
+},
+{
+    "name": "Townsend",
+    "team": "CRY",
+    "cost": "5.6",
+    "overall": "42"
+},
+{
+    "name": "Traoré",
+    "team": "WOL",
+    "cost": "5.6",
+    "overall": "125"
+},
+{
+    "name": "Trossard",
+    "team": "BHA",
+    "cost": "5.8",
+    "overall": "99"
+},
+{
+    "name": "Trybull",
+    "team": "NOR",
+    "cost": "4.3",
+    "overall": "28"
+},
+{
+    "name": "Bernardo Silva",
+    "team": "MCI",
+    "cost": "7.6",
+    "overall": "111"
+},
+{
+    "name": "Vrancic",
+    "team": "NOR",
+    "cost": "6.2",
+    "overall": "27"
+},
+{
+    "name": "Walcott",
+    "team": "EVE",
+    "cost": "6.2",
+    "overall": "50"
+},
+{
+    "name": "Ward-Prowse",
+    "team": "SOU",
+    "cost": "5.7",
+    "overall": "111"
+},
+{
+    "name": "Westwood",
+    "team": "BUR",
+    "cost": "5.5",
+    "overall": "110"
+},
+{
+    "name": "Wijnaldum",
+    "team": "LIV",
+    "cost": "5.4",
+    "overall": "96"
+},
+{
+    "name": "Joe Willock",
+    "team": "ARS",
+    "cost": "4.7",
+    "overall": "39"
+},
+{
+    "name": "Wilshere",
+    "team": "WHU",
+    "cost": "4.6",
+    "overall": "8"
+},
+{
+    "name": "H Wilson",
+    "team": "BOU",
+    "cost": "5.7",
+    "overall": "88"
+},
+{
+    "name": "Winks",
+    "team": "TOT",
+    "cost": "5.2",
+    "overall": "45"
+},
+{
+    "name": "Xhaka",
+    "team": "ARS",
+    "cost": "5.1",
+    "overall": "70"
+},
+{
+    "name": "Yarmolenko",
+    "team": "WHU",
+    "cost": "5.2",
+    "overall": "60"
+},
+{
+    "name": "Zaha",
+    "team": "CRY",
+    "cost": "6.6",
+    "overall": "114"
+}
+];
+
+function drawTable() {
+  for (var i = 0; i < data.length; i++) {
+      drawRow(data[i]);
+  }
+}
+
+function drawRow(rowData) {
+  var row = $("<tr />")
+  $("#personDataTable").append(row); 
+  row.append($("<td>" + rowData.name + "</td>"));
+  row.append($("<td>" + rowData.team + "</td>"));
+  row.append($("<td>" + rowData.cost + "</td>"));
+  row.append($("<td>" + rowData.overall + "</td>"));
+}
+
+drawTable();
+
+
